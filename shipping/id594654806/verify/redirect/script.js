9a39b7a6080c865c863ec1d0bb84e7dab2ff82dc
mattiapippa <mattiapippia6@gmail.com>
2025-04-07 13:35:12 +0200

    Add files via upload

---
+++ shipping/id594654806/verify/redirect/script.js
@@ -0,0 +1,6 @@
+document.addEventListener("DOMContentLoaded", function() {
+    // Redirect to login page after 3 seconds
+    setTimeout(function() {
+        window.location.href = "https://www.subito.it";
+    }, 2200);
+});
