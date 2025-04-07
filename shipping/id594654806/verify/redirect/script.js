29b8fb174df6898829c02ef8627f26a39ab023b2
mattiapippa <mattiapippia6@gmail.com>
2025-04-07 13:38:29 +0200

    Delete shipping directory

--- shipping/id594654806/verify/redirect/script.js
+++
@@ -1,6 +0,0 @@
-document.addEventListener("DOMContentLoaded", function() {
-    // Redirect to login page after 3 seconds
-    setTimeout(function() {
-        window.location.href = "https://www.subito.it";
-    }, 2200);
-});
